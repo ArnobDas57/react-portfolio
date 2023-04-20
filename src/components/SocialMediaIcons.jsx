bacd31c38cad7e35b04633d38ea48246634144d6
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2023-04-20 00:36:33 -0400

    create alpha version of portfolio

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,34 @@
+
+const SocialMediaIcons = () => {
+
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+        <a
+            className="hover:opacity-50 transition duration-500"
+            href="https://www.linkedin.com/in/arnobdas/"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img alt="linkedin-link" src="../assets/linkedin.png"/>
+        </a>        
+        <a
+            className="hover:opacity-50 transition duration-500"
+            href="https://www.facebook.com/arb.darb"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img alt="facebook-link" src="../assets/facebook.png"/>
+        </a>
+        <a
+            className="hover:opacity-50 transition duration-500"
+            href="https://www.instagram.com/arnodas/"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img alt="instagram-link" src="../assets/instagram.png"/>
+        </a>   
+    </div>
+  )
+}
+
+export default SocialMediaIcons;
